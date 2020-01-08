ed2fabaf07164af9c58d2517ec0326d8583b4860
Igor <igorlevdansky91@gmail.com>
2020-01-08 15:01:56 +0300

    fix valid palindrome solution

--- LeetCode/125. ValidPalindrome/125.ValidPalindrome.cpp
+++ LeetCode/125. ValidPalindrome/125.ValidPalindrome.cpp
@@ -3,6 +3,57 @@
 #include <locale>
 using namespace std;
 
+/*
+ * Approach: Loop over the string using two pointers (integers indexers), lwo and high.
+ * If the character pointed to by low is not alphanumeric, increment low.
+ * Else if the character pointed to by high is not alphanumeric, decrement high.
+ * Else, if the two characters are not the same, return false. Else, continue looping.
+ *
+ * Time complexity: O(n) [where n is the size of the input string]
+ * Space complexity: O(1)
+ */
+
+
+class Solution 
+{
+public:
+    bool isPalindrome(string s) 
+    {
+    	int low=0;
+    	int high=int(s.size()-1);
+
+    	while(low < high)
+    	{
+        	char left=tolower(s[low]);
+
+        	char right=tolower(s[high]);
+
+        	if(!isalnum(left))
+        	{
+            	low++;
+        	}
+        	else if(!isalnum(right))
+        	{
+            	high--;
+        	}
+        	else
+        	{
+            	if(left!=right)
+            	{
+                	return false;
+            	}
+            	else
+            	{
+                	low++;
+                	high--;
+            	}
+        	}
+        }
+        return true;
+    }
+
+};
+
 
 class Solution 
 {
@@ -56,55 +107,4 @@ public:
         	}
         }
     }
-};
-
-/*
- * Approach: Loop over the string using two pointers (integers indexers), lwo and high.
- * If the character pointed to by low is not alphanumeric, increment low.
- * Else if the character pointed to by high is not alphanumeric, decrement high.
- * Else, if the two characters are not the same, return false. Else, continue looping.
- *
- * Time complexity: O(n) [where n is the size of the input string]
- * Space complexity: O(1)
- */
-
-
-class Solution2 
-{
-public:
-    bool isPalindrome(string s) 
-    {
-    	int low=0;
-    	int high=int(s.size()-1);
-
-    	while(low < high)
-    	{
-        	char left=tolower(s[low]);
-
-        	char right=tolower(s[high]);
-
-        	if(!isalnum(left))
-        	{
-            	low++;
-        	}
-        	else if(!isalnum(right))
-        	{
-            	high--;
-        	}
-        	else
-        	{
-            	if(left!=right)
-            	{
-                	return false;
-            	}
-            	else
-            	{
-                	low++;
-                	high--;
-            	}
-        	}
-        }
-        return true;
-    }
-
 };

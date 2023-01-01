76aed388bebbca9328b993611ec9fea57ca0e188
Igor Levdansky <igorlevdansky91@gmail.com>
2023-01-01 19:38:36 +0300

    Add SortArrayByParity (add solution)

--- LeetCode/905. SortArrayByParity/905. SortArrayByParity.cpp
+++ LeetCode/905. SortArrayByParity/905. SortArrayByParity.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
+
 
 //runtime 28 ms, memory usage 9.5 mb
 class Solution 
 {
 public:
-    vector<int> sortArrayByParity(vector<int>& a) 
+    std::vector<int> sortArrayByParity(std::vector<int>& a) 
     {
         int i = 0;
         int j = (int)a.size() - 1;
@@ -23,7 +23,7 @@ public:
         	}
         	if(i < j)
         	{
-        		swap(a[i], a[j]);
+        		std::swap(a[i], a[j]);
         		i++;
         		j--;
         	}
@@ -32,6 +32,39 @@ public:
         		break;
         	}
         }
-        return move(a);
+        return std::move(a);
     }
 };
+
+
+class Solution
+{
+ 	public:
+    [[nodiscard]] std::vector<int> sortArrayByParity(std::vector<int> const& nums) const
+    {
+        std::vector<int> result(nums.size(), 0);
+        size_t left = 0;
+        size_t right = result.size() - 1;
+        for (const int num : nums)
+        {
+        	if (num % 2 == 0)
+                result[left++] = num;
+            else
+                result[right--] = num;
+        }
+    	return result;
+    }
+};
+
+
+int main(int argc, char** argv)
+{
+    Solution s;
+    std::vector<int> v{ 1,2,3,4 };
+    for (auto i : s.sortArrayByParity(v))
+    {
+        std::cerr << i << '\t';
+    }
+
+    return 0;
+}
